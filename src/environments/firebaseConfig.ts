7107e42dc66b847c6c25ce44bc1884d3f6c14eb8
Jonathan Jesús Herrera López <jonathan_herrera117@hotmail.com>
2022-06-04 21:05:41 -0500

    Added firebaseConfig

---
+++ src/environments/firebaseConfig.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCGMzkc8kyDaUW6_uG99nRKsPAplrS1Huk",
+  authDomain: "becas-app.firebaseapp.com",
+  projectId: "becas-app",
+  storageBucket: "becas-app.appspot.com",
+  messagingSenderId: "439159812338",
+  appId: "1:439159812338:web:9439b05c389f8e8c54a06b",
+  measurementId: "G-B7YLKFE6XB"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
