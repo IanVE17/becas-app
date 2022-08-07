c7cc6203f2248b9de17054f43b6742c9624b5a4e
Jonathan Jesús Herrera López <jonathan_herrera117@hotmail.com>
2022-08-06 20:51:57 -0500

    Removed firebase info, app deleted

--- src/environments/firebaseConfig.js
+++ src/environments/firebaseConfig.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCGMzkc8kyDaUW6_uG99nRKsPAplrS1Huk",
-  authDomain: "becas-app.firebaseapp.com",
-  projectId: "becas-app",
-  storageBucket: "becas-app.appspot.com",
-  messagingSenderId: "439159812338",
-  appId: "1:439159812338:web:9439b05c389f8e8c54a06b",
-  measurementId: "G-B7YLKFE6XB",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 // Initialize Firebase
